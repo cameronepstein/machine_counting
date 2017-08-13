472e4d22585f2df2fe3dd53c31b5ef2136103dd4
Cameron Epstein <cameron.a.epstein@gmail.com>
2017-08-13 19:00:59 +0100

    count to five

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
-var countToNumber = (maxNumber) => {
-  console.log(findRandomNumber(maxNumber));
+'use strict'
+
+// Notification that count has not worked
+const errors = {
+  countError: 'Error: incorrect number in sequence'
 }
 
-findRandomNumber = (number) => {
-  return Math.floor(Math.random() * number) + 1
+var countBetweenNumbers = (minNumber, maxNumber) => {
+  let firstNumberChoice = findRandomNumber(maxNumber);
+  if (minNumber == maxNumber - 1 && firstNumberChoice === maxNumber) {
+    console.log(firstNumberChoice);
+    console.log(`counted to ${maxNumber}`);
+  } else if (firstNumberChoice - minNumber == 1) {
+    console.log(firstNumberChoice);
+    countBetweenNumbers(firstNumberChoice, maxNumber);
+    return;
+  } else {
+    console.log(firstNumberChoice);
+    console.log(errors.countError);
+    startFromBeginning();
+    return;
+  }
 }
 
-countToNumber(10);
+var findRandomNumber = (number) => {
+  let randomNumber = Math.floor(Math.random() * number) + 1;
+  return randomNumber;
+}
+
+var startFromBeginning = () => {
+  countBetweenNumbers(0, 5);
+}
+
+startFromBeginning();
