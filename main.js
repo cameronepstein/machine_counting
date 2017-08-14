4b8a9f0fe5b640e1d38b7f752c716d9f68ce0386
Cameron Epstein <cameron.a.epstein@gmail.com>
2017-08-14 09:33:41 +0100

    add comments

--- main.js
+++ main.js
@@ -4,16 +4,19 @@
 const errors = {
   countError: 'Error: incorrect number in sequence'
 }
-
+// Attempt at replicating idea in my head of what a machine learning count algorithm might look like
 var countBetweenNumbers = (minNumber, maxNumber) => {
   let firstNumberChoice = findRandomNumber(maxNumber);
+  // If count reaches max number
   if (minNumber == maxNumber - 1 && firstNumberChoice === maxNumber) {
     console.log(firstNumberChoice);
     console.log(`counted to ${maxNumber}`);
+  // If count is correct
   } else if (firstNumberChoice - minNumber == 1) {
     console.log(firstNumberChoice);
     countBetweenNumbers(firstNumberChoice, maxNumber);
     return;
+    // If count is incorrect
   } else {
     console.log(firstNumberChoice);
     console.log(errors.countError);
@@ -22,11 +25,13 @@ var countBetweenNumbers = (minNumber, maxNumber) => {
   }
 }
 
+// Get random number between 1 and 'number'
 var findRandomNumber = (number) => {
   let randomNumber = Math.floor(Math.random() * number) + 1;
   return randomNumber;
 }
 
+// Begin counting sequence
 var startFromBeginning = () => {
   countBetweenNumbers(0, 5);
 }
